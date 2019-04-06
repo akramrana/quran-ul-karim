a28213612cb17970b2c2e71d860e40a9f8cfb67f
Akram Hossain <akram686202@gmail.com>
2019-04-07 00:35:39 +0600

    res

---
+++ java/com/codxplore/quranulkarim/QuickLinksActivity.java
@@ -0,0 +1,13 @@
+package com.codxplore.quranulkarim;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuickLinksActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quick_links);
+    }
+}
