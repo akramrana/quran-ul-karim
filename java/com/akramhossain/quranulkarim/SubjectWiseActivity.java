dfc3d693df4b20c8632a7e92347ee8f7508b2b2e
Akram Hossain <akram686202@gmail.com>
2023-07-18 11:46:03 +0600

    bug fix

--- java/com/akramhossain/quranulkarim/SubjectWiseActivity.java
+++ java/com/akramhossain/quranulkarim/SubjectWiseActivity.java
@@ -1,14 +1,74 @@
 package com.akramhossain.quranulkarim;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.akramhossain.quranulkarim.adapter.QuranIndexViewAdapter;
+import com.akramhossain.quranulkarim.helper.DatabaseHelper;
+import com.akramhossain.quranulkarim.model.QuranIndex;
+
+import java.util.ArrayList;
 
 public class SubjectWiseActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerview;
+    private QuranIndexViewAdapter rvAdapter;
+    LinearLayoutManager mLayoutManager;
+    private ArrayList<QuranIndex> quranIndexes;
+
+    private static final String TAG = SubjectWiseActivity.class.getSimpleName();
+    DatabaseHelper dbhelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_wise);
+        recyclerview = (RecyclerView) findViewById(R.id.all_index_list);
+        mLayoutManager = new LinearLayoutManager(this);
+        recyclerview.setLayoutManager(mLayoutManager);
+        setRecyclerViewAdapter();
+        dbhelper = DatabaseHelper.getInstance(getApplicationContext());
+        getDataFromLocalDb();
+    }
+
+    private void getDataFromLocalDb() {
+        SQLiteDatabase db = DatabaseHelper.getInstance(getApplicationContext()).getWritableDatabase();
+        String sql = sql = "SELECT quran_index.* " +
+                "FROM quran_index " +
+                "order by en ASC";
+        Log.i(TAG, sql);
+        Cursor cursor = db.rawQuery(sql, null);
+        try {
+            if (cursor.moveToFirst()) {
+                do {
+                    QuranIndex qi = new QuranIndex();
+                    qi.setBn(cursor.getString(cursor.getColumnIndexOrThrow("bn")));
+                    qi.setEn(cursor.getString(cursor.getColumnIndexOrThrow("en")));
+                    qi.setTags(cursor.getString(cursor.getColumnIndexOrThrow("tags")));
+                    qi.setVerses(cursor.getString(cursor.getColumnIndexOrThrow("verses")));
+                    quranIndexes.add(qi);
+                } while (cursor.moveToNext());
+            }
+        }catch (Exception e){
+            Log.i(TAG, e.getMessage());
+        }
+        finally {
+            if (cursor != null && !cursor.isClosed()){
+                cursor.close();
+            }
+            db.close();
+        }
+        rvAdapter.notifyDataSetChanged();
+    }
+
+    private void setRecyclerViewAdapter() {
+        quranIndexes = new ArrayList<QuranIndex>();
+        rvAdapter = new QuranIndexViewAdapter(SubjectWiseActivity.this, quranIndexes, this);
+        recyclerview.setAdapter(rvAdapter);
     }
 }
--- java/com/akramhossain/quranulkarim/adapter/QuranIndexViewAdapter.java
+++ java/com/akramhossain/quranulkarim/adapter/QuranIndexViewAdapter.java
@@ -1,4 +1,98 @@
 package com.akramhossain.quranulkarim.adapter;
 
-public class QuranIndexViewAdapter {
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.preference.PreferenceManager;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.akramhossain.quranulkarim.R;
+import com.akramhossain.quranulkarim.model.QuranIndex;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class QuranIndexViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+    Context c;
+    ArrayList<QuranIndex> quranIndex;
+    private Activity activity;
+    Typeface font, fontUthmani, fontAlmajeed, fontAlQalam, fontNooreHidayat, fontSaleem;
+    SharedPreferences mPrefs;
+
+    public QuranIndexViewAdapter(Context c, ArrayList<QuranIndex> quranIndex, Activity activity) {
+        this.c = c;
+        this.quranIndex = quranIndex;
+        this.activity = activity;
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        font = Typeface.createFromAsset(c.getAssets(), "fonts/Siyamrupali.ttf");
+        fontUthmani = Typeface.createFromAsset(c.getAssets(),"fonts/KFGQPC_Uthmanic_Script_HAFS_Regular.ttf");
+        fontAlmajeed = Typeface.createFromAsset(c.getAssets(),"fonts/AlMajeedQuranicFont_shiped.ttf");
+        fontAlQalam = Typeface.createFromAsset(c.getAssets(),"fonts/AlQalamQuran.ttf");
+        fontNooreHidayat = Typeface.createFromAsset(c.getAssets(),"fonts/noorehidayat.ttf");
+        fontSaleem = Typeface.createFromAsset(c.getAssets(),"fonts/PDMS_Saleem_QuranFont.ttf");
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(c).inflate(R.layout.quran_index_list,parent,false);
+        RecyclerViewHolder rvHolder = new RecyclerViewHolder(v);
+        rvHolder.bn.setTypeface(font);
+        return rvHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        RecyclerViewHolder rvHolder= (RecyclerViewHolder) holder;
+        QuranIndex qi = quranIndex.get(position);
+        rvHolder.en.setText(qi.getEn());
+        rvHolder.bn.setText(qi.getBn());
+        //rvHolder.verses.setText(qi.getVerses());
+        //rvHolder.tags.setText(qi.getTags());
+        String verses = qi.getVerses();
+        String[] ayahList = verses.split(",");
+        rvHolder.ayah_num.setText("No.of Ayah: "+ayahList.length);
+    }
+
+    @Override
+    public int getItemCount() {
+        return quranIndex.size();
+    }
+
+    class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+        TextView bn;
+        //TextView tags;
+        TextView en;
+        //TextView verses;
+        TextView ayah_num;
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+            String mp_enFz = mPrefs.getString("enFontSize", "15");
+            String mp_bnFz = mPrefs.getString("bnFontSize", "15");
+
+            bn = (TextView) itemView.findViewById(R.id.bn);
+            //tags = (TextView) itemView.findViewById(R.id.tags);
+            en = (TextView) itemView.findViewById(R.id.en);
+            //verses = (TextView) itemView.findViewById(R.id.verses);
+            ayah_num = (TextView) itemView.findViewById(R.id.ayah_count);
+
+            if (!mp_bnFz.equals("")) {
+                bn.setTextSize(TypedValue.COMPLEX_UNIT_DIP, Integer.parseInt(mp_bnFz));
+            }
+            if (!mp_enFz.equals("")) {
+                en.setTextSize(TypedValue.COMPLEX_UNIT_DIP, Integer.parseInt(mp_enFz));
+            }
+        }
+
+    }
+
 }
