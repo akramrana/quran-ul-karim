f39291942ea757030df6d31c130203795be41018
Akram Hossain <akram686202@gmail.com>
2023-07-18 14:25:34 +0600

    new changes

--- java/com/akramhossain/quranulkarim/SubjectWiseActivity.java
+++ java/com/akramhossain/quranulkarim/SubjectWiseActivity.java
@@ -7,9 +7,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.akramhossain.quranulkarim.adapter.QuranIndexViewAdapter;
 import com.akramhossain.quranulkarim.helper.DatabaseHelper;
@@ -18,7 +22,7 @@ import com.akramhossain.quranulkarim.model.QuranIndex;
 
 import java.util.ArrayList;
 
-public class SubjectWiseActivity extends AppCompatActivity {
+public class SubjectWiseActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
 
     private RecyclerView recyclerview;
     private QuranIndexViewAdapter rvAdapter;
@@ -27,6 +31,14 @@ public class SubjectWiseActivity extends AppCompatActivity {
 
     private static final String TAG = SubjectWiseActivity.class.getSimpleName();
     DatabaseHelper dbhelper;
+
+    SearchView editsearch;
+
+    String searchTxt = "";
+
+    Handler mHandler = new Handler();
+
+    Typeface font;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,15 +66,33 @@ public class SubjectWiseActivity extends AppCompatActivity {
             }
         }));
 
+        font = Typeface.createFromAsset(getApplicationContext().getAssets(),"fonts/Siyamrupali.ttf");
+
         dbhelper = DatabaseHelper.getInstance(getApplicationContext());
         getDataFromLocalDb();
+
+        editsearch = (SearchView) findViewById(R.id.search);
+        editsearch.setOnQueryTextListener(this);
+
+        TextView hint = (TextView) findViewById(R.id.hint);
+        hint.setTypeface(font);
     }
 
     private void getDataFromLocalDb() {
         SQLiteDatabase db = DatabaseHelper.getInstance(getApplicationContext()).getWritableDatabase();
-        String sql = sql = "SELECT quran_index.* " +
-                "FROM quran_index " +
-                "order by en ASC";
+        String sql = "";
+        searchTxt = searchTxt.replaceAll("\'","");
+        Log.d("Search Term",searchTxt);
+        if(searchTxt.equals("")) {
+            sql = "SELECT quran_index.* " +
+                    "FROM quran_index " +
+                    "order by en ASC";
+        }else{
+            sql = "SELECT quran_index.* " +
+                    "FROM quran_index " +
+                    "WHERE en LIKE '%"+searchTxt+"%' OR bn LIKE '%"+searchTxt+"%' "+
+                    "order by en ASC";
+        }
         Log.i(TAG, sql);
         Cursor cursor = db.rawQuery(sql, null);
         try {
@@ -86,6 +116,38 @@ public class SubjectWiseActivity extends AppCompatActivity {
             db.close();
         }
         rvAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String query) {
+        searchTxt = query;
+        mHandler.removeCallbacksAndMessages(null);
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                int length = searchTxt.length();
+                if(length > 0) {
+                    quranIndexes = new ArrayList<QuranIndex>();
+                    rvAdapter = new QuranIndexViewAdapter(SubjectWiseActivity.this, quranIndexes, SubjectWiseActivity.this);
+                    recyclerview.setAdapter(rvAdapter);
+                    getDataFromLocalDb();
+                }
+                else{
+                    quranIndexes = new ArrayList<QuranIndex>();
+                    rvAdapter = new QuranIndexViewAdapter(SubjectWiseActivity.this, quranIndexes, SubjectWiseActivity.this);
+                    recyclerview.setAdapter(rvAdapter);
+                    getDataFromLocalDb();
+                }
+            }
+        }, 100);
+
+
+        return true;
     }
 
     private void setRecyclerViewAdapter() {
