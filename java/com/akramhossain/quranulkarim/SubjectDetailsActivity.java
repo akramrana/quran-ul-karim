5498dc08c034557ce4085ecaac6a60fb79de4176
Akram Hossain <akram686202@gmail.com>
2023-07-18 12:28:22 +0600

    new changes

---
+++ java/com/akramhossain/quranulkarim/SubjectDetailsActivity.java
@@ -0,0 +1,198 @@
+package com.akramhossain.quranulkarim;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.akramhossain.quranulkarim.adapter.JuzHizbRubViewAdapter;
+import com.akramhossain.quranulkarim.helper.AudioPlay;
+import com.akramhossain.quranulkarim.helper.DatabaseHelper;
+import com.akramhossain.quranulkarim.model.Ayah;
+
+import java.util.ArrayList;
+
+public class SubjectDetailsActivity extends AppCompatActivity {
+
+    TextView title,subtitle;
+    String enTxt, BnTxt, verses;
+
+    private RecyclerView recyclerview;
+
+    LinearLayoutManager mLayoutManager;
+
+    private ArrayList<Ayah> ayahs;
+
+    private static final String TAG = SubjectDetailsActivity.class.getSimpleName();
+
+    DatabaseHelper dbhelper;
+
+    private JuzHizbRubViewAdapter rvAdapter;
+
+    private boolean itShouldLoadMore = true;
+
+    Integer offset = 0;
+
+    Integer limit = 100;
+
+    Integer counter = 0;
+
+    String where_clause = "";
+
+    private static final int PERMISSION_REQUEST_CODE = 100;
+
+    SharedPreferences mPrefs;
+
+    Typeface fontUthmani, fontAlmajeed, fontAlQalam, fontNooreHidayat, fontSaleem, font;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_details);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            enTxt = extras.getString("en");
+            BnTxt = extras.getString("bn");
+            verses = extras.getString("verses");
+        }
+
+        title = (TextView) findViewById(R.id.title);
+        title.setText(enTxt);
+
+        subtitle = (TextView) findViewById(R.id.subtitle);
+        subtitle.setText(BnTxt);
+
+        recyclerview = (RecyclerView) findViewById(R.id.ayah_list);
+        mLayoutManager = new LinearLayoutManager(this);
+        recyclerview.setLayoutManager(mLayoutManager);
+
+        setRecyclerViewAdapter();
+
+        dbhelper = DatabaseHelper.getInstance(getApplicationContext());
+
+        String verseList = verses;
+        String[] ayahList = verseList.split(",");
+
+        ArrayList<String> wheresArr = new ArrayList<String>();
+        for(String ayahKey : ayahList){
+            wheresArr.add("ayah.ayah_key = '"+ayahKey.trim()+"'");
+        }
+        where_clause = String.join(" or ", wheresArr);
+
+        //System.out.println(where_clause);
+        getDataFromLocalDb();
+
+        if (checkPermission()) {
+
+        }else{
+            requestPermission();
+        }
+
+        font = Typeface.createFromAsset(getApplicationContext().getAssets(),"fonts/Siyamrupali.ttf");
+        subtitle.setTypeface(font);
+    }
+
+    private void setRecyclerViewAdapter() {
+        ayahs = new ArrayList<Ayah>();
+        rvAdapter = new JuzHizbRubViewAdapter(SubjectDetailsActivity.this, ayahs, this);
+        recyclerview.setAdapter(rvAdapter);
+    }
+
+    private void getDataFromLocalDb() {
+        SQLiteDatabase db = DatabaseHelper.getInstance(getApplicationContext()).getWritableDatabase();
+        String sql = String.format("SELECT ayah.*,sura.name_simple,sura.name_complex,sura.name_english,sura.name_arabic,transliteration.trans,ayah_indo.text as indo_pak " +
+                "FROM ayah " +
+                "LEFT JOIN sura ON ayah.surah_id = sura.surah_id " +
+                "LEFT join transliteration ON ayah.ayah_num = transliteration.ayat_id and transliteration.sura_id = ayah.surah_id " +
+                "LEFT join ayah_indo ON ayah.ayah_num = ayah_indo.ayah and ayah_indo.sura = ayah.surah_id " +
+                "WHERE %s order by ayah_index ASC ", where_clause);
+        Log.i(TAG, sql);
+        Cursor cursor = db.rawQuery(sql, null);
+        try {
+            if (cursor.moveToFirst()) {
+                do {
+                    Ayah ayah = new Ayah();
+                    ayah.setAyah_index(cursor.getString(cursor.getColumnIndexOrThrow("ayah_index")));
+                    ayah.setSurah_id(cursor.getString(cursor.getColumnIndexOrThrow("surah_id")));
+                    ayah.setAyah_num(cursor.getString(cursor.getColumnIndexOrThrow("ayah_num")));
+                    ayah.setPage_num(cursor.getString(cursor.getColumnIndexOrThrow("page_num")));
+                    ayah.setJuz_num(cursor.getString(cursor.getColumnIndexOrThrow("juz_num")));
+                    ayah.setHizb_num(cursor.getString(cursor.getColumnIndexOrThrow("hizb_num")));
+                    ayah.setRub_num(cursor.getString(cursor.getColumnIndexOrThrow("rub_num")));
+                    ayah.setText(cursor.getString(cursor.getColumnIndexOrThrow("text")));
+                    ayah.setAyah_key(cursor.getString(cursor.getColumnIndexOrThrow("ayah_key")));
+                    ayah.setSajdah(cursor.getString(cursor.getColumnIndexOrThrow("sajdah")));
+                    ayah.setText_tashkeel(cursor.getString(cursor.getColumnIndexOrThrow("text_tashkeel")));
+                    ayah.setContent_en(cursor.getString(cursor.getColumnIndexOrThrow("content_en")));
+                    ayah.setContent_bn(cursor.getString(cursor.getColumnIndexOrThrow("content_bn")));
+                    ayah.setAudio_duration(cursor.getString(cursor.getColumnIndexOrThrow("audio_duration")));
+                    ayah.setAudio_url(cursor.getString(cursor.getColumnIndexOrThrow("audio_url")));
+                    ayah.setName_simple(cursor.getString(cursor.getColumnIndexOrThrow("name_simple")));
+                    ayah.setName_complex(cursor.getString(cursor.getColumnIndexOrThrow("name_complex")));
+                    ayah.setName_english(cursor.getString(cursor.getColumnIndexOrThrow("name_english")));
+                    ayah.setName_arabic(cursor.getString(cursor.getColumnIndexOrThrow("name_arabic")));
+                    ayah.setTrans(cursor.getString(cursor.getColumnIndexOrThrow("trans")));
+                    ayah.setIndo_pak(cursor.getString(cursor.getColumnIndexOrThrow("indo_pak")));
+                    ayahs.add(ayah);
+                } while (cursor.moveToNext());
+            }
+        }catch (Exception e){
+            Log.i(TAG, e.getMessage());
+        }
+        finally {
+            if (cursor != null && !cursor.isClosed()){
+                cursor.close();
+            }
+            db.close();
+        }
+        rvAdapter.notifyDataSetChanged();
+    }
+
+    public void onPause()
+    {
+        super.onPause();
+        AudioPlay.stopAudio();
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(SubjectDetailsActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(SubjectDetailsActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(SubjectDetailsActivity.this, "Write External Storage permission allows us to save files. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(SubjectDetailsActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    Log.e("value", "Permission Denied, You cannot use local drive .");
+                }
+                break;
+        }
+    }
+}
--- java/com/akramhossain/quranulkarim/SubjectWiseActivity.java
+++ java/com/akramhossain/quranulkarim/SubjectWiseActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.akramhossain.quranulkarim.adapter.QuranIndexViewAdapter;
 import com.akramhossain.quranulkarim.helper.DatabaseHelper;
+import com.akramhossain.quranulkarim.listener.RecyclerTouchListener;
 import com.akramhossain.quranulkarim.model.QuranIndex;
 
 import java.util.ArrayList;
@@ -32,6 +35,25 @@ public class SubjectWiseActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         recyclerview.setLayoutManager(mLayoutManager);
         setRecyclerViewAdapter();
+
+        recyclerview.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerview, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                QuranIndex qi = quranIndexes.get(position);
+                //Toast.makeText(getApplicationContext(), vd.getVideo_id() + " is selected!", Toast.LENGTH_SHORT).show();
+                Intent in = new Intent(getApplicationContext(),SubjectDetailsActivity.class);
+                in.putExtra("en", qi.getEn());
+                in.putExtra("bn", qi.getBn());
+                in.putExtra("tags", qi.getTags());
+                in.putExtra("verses", qi.getVerses());
+                startActivityForResult(in, 100);
+            }
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+
         dbhelper = DatabaseHelper.getInstance(getApplicationContext());
         getDataFromLocalDb();
     }
