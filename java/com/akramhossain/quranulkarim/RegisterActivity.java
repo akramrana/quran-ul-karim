38f754a559bf86d8902641a9b5a2e2cf24fd62c0
akram_rana <akram686202@gmail.com>
2024-10-28 19:32:14 +0600

    changes

--- java/com/akramhossain/quranulkarim/RegisterActivity.java
+++ java/com/akramhossain/quranulkarim/RegisterActivity.java
@@ -1,24 +1,79 @@
 package com.akramhossain.quranulkarim;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import com.akramhossain.quranulkarim.app.AppController;
+import com.akramhossain.quranulkarim.helper.SessionManager;
+import com.android.volley.toolbox.StringRequest;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = RegisterActivity.class.getSimpleName();
+    private Button btnRegister;
+    private Button btnLinkToLoginScreen;
+    private EditText inputFullName;
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private SessionManager session;
+    ProgressBar progressBar;
+    public static String host = "http://quran.codxplore.com/";
+    public static String URL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Button btnLinkToLoginScreen = (Button) findViewById(R.id.btnLinkToLoginScreen);
+        inputFullName = (EditText) findViewById(R.id.name);
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        session = new SessionManager(getApplicationContext());
+        if (session.isLoggedIn()) {
+            // User is already logged in. Take him to main activity
+            Intent intent = new Intent(RegisterActivity.this,LeaderboardActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        URL = host+"api/v1/register.php";
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String name = inputFullName.getText().toString().trim();
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+                if (!name.isEmpty() && !email.isEmpty() && !password.isEmpty()) {
+                    registerUser(name, email, password);
+                } else {
+                    Toast.makeText(getApplicationContext(),"Please enter your details!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnLinkToLoginScreen = (Button) findViewById(R.id.btnLinkToLoginScreen);
         btnLinkToLoginScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,5 +81,58 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    private void registerUser(final String name, final String email,final String password) {
+        // Tag used to cancel the request
+        String tag_string_req = "req_register";
+        progressBar.setVisibility(View.VISIBLE);
+        Log.d(TAG, "Register URL: " + URL.toString());
+        StringRequest strReq = new StringRequest(Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Register Response: " + response.toString());
+                progressBar.setVisibility(View.GONE);
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+                    if (!error) {
+                        // User successfully stored in database
+                        Toast.makeText(getApplicationContext(), "User successfully registered. Try login now!", Toast.LENGTH_LONG).show();
+                        // Launch login activity
+                        Intent intent = new Intent(RegisterActivity.this,SigninActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        // Error occurred in registration. Get the error
+                        // message
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Registration Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to register url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", name);
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+        };
+        strReq.setShouldCache(false);
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
     }
 }
