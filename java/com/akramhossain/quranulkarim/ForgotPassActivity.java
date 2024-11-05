896b7de1fe9bf62c95c447366b73ce781501e0cf
akram_rana <akram686202@gmail.com>
2024-11-05 13:12:04 +0600

    changes

--- java/com/akramhossain/quranulkarim/ForgotPassActivity.java
+++ java/com/akramhossain/quranulkarim/ForgotPassActivity.java
@@ -2,8 +2,25 @@ package com.akramhossain.quranulkarim;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.akramhossain.quranulkarim.app.AppController;
+import com.akramhossain.quranulkarim.helper.SessionManager;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,10 +30,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ForgotPassActivity extends AppCompatActivity {
 
+    private static final String TAG = ForgotPassActivity.class.getSimpleName();
+    private EditText inputEmail;
+    ProgressBar progressBar;
+    private SessionManager session;
+    public static String URL;
+    public static String host = "http://quran.codxplore.com/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_pass);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        session = new SessionManager(getApplicationContext());
+        if (session.isLoggedIn()) {
+            // User is already logged in. Take him to main activity
+            Intent intent = new Intent(ForgotPassActivity.this,LeaderboardActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
         Button btnLinkToLoginScreen = (Button) findViewById(R.id.btnLinkToLoginScreen);
         btnLinkToLoginScreen.setOnClickListener(new View.OnClickListener() {
@@ -27,5 +60,70 @@ public class ForgotPassActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        URL = host+"api/v1/forgot-password.php";
+
+        inputEmail = (EditText) findViewById(R.id.email);
+
+        Button btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String email = inputEmail.getText().toString().trim();
+                if (!email.isEmpty()) {
+                    resetPass(email);
+                } else {
+                    Toast.makeText(getApplicationContext(),"Please enter your details!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
+    private void resetPass(final String email) {
+        // Tag used to cancel the request
+        String tag_string_req = "req_reset_pass";
+        progressBar.setVisibility(View.VISIBLE);
+        Log.d(TAG, "Reset Pass URL: " + URL.toString());
+        StringRequest strReq = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Reset Pass Response: " + response.toString());
+                progressBar.setVisibility(View.GONE);
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+                    if (!error) {
+                        // User successfully stored in database
+                        Toast.makeText(getApplicationContext(), "A password reset token has been sent to your email. Please check your inbox/spam folder!", Toast.LENGTH_LONG).show();
+                    } else {
+                        // Error occurred in registration. Get the error
+                        // message
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Reset Pass Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),error.getMessage(), Toast.LENGTH_LONG).show();
+                progressBar.setVisibility(View.GONE);
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to register url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email);
+                return params;
+            }
+        };
+        strReq.setShouldCache(false);
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
     }
 }
