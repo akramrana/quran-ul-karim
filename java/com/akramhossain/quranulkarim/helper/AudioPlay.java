cce2f6faa08ea16e4b82b55a5a4dec83849115ae
akram_rana <akram686202@gmail.com>
2024-12-30 20:06:27 +0600

    changes

--- java/com/akramhossain/quranulkarim/helper/AudioPlay.java
+++ java/com/akramhossain/quranulkarim/helper/AudioPlay.java
@@ -5,142 +5,118 @@ import android.media.MediaPlayer;
 import android.util.Log;
 
 public class AudioPlay {
-    public static boolean isAudioPlaying = false;
-    public static boolean isAudioLoaded = false;
-    public static boolean isAudioStopped = false;
-    public static MediaPlayer mp;
-    public static String mp3Uri;
 
-    public static void playAudio(Context context, String audioUri)
-    {
-        mp = new MediaPlayer();
-        try
-        {
-            if (mp != null && mp.isPlaying())
-            {
-                mp.stop();
-                mp.release();
-                mp = null;
-                return;
-            }
-            mp.setDataSource(audioUri);
-            mp.prepare();
-            int i = mp.getCurrentPosition();
-            mp.seekTo(i);
+    private static MediaPlayer mp;
+    private static boolean isAudioPlaying = false;
+    private static boolean isAudioLoaded = false;
+    private static boolean isAudioStopped = false;
+    private static String mp3Uri;
+
+    private AudioPlay() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static synchronized void playAudio(Context context, String audioUri) {
+        try {
+            initializeMediaPlayer(audioUri);
             mp.start();
             isAudioPlaying = true;
             isAudioLoaded = true;
             isAudioStopped = false;
             mp3Uri = audioUri;
-            return;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             Log.e("MP3", e.toString());
         }
     }
 
-    public static void prepareAudio(Context context, String audioUri){
-        mp = new MediaPlayer();
-        try{
-            if (mp != null && mp.isPlaying())
-            {
-                mp.stop();
-                mp.release();
-                mp = null;
-                return;
-            }
-            mp.setDataSource(audioUri);
-            mp.prepare();
-            return;
-        }
-        catch (Exception e)
-        {
+    public static synchronized void prepareAudio(Context context, String audioUri) {
+        try {
+            initializeMediaPlayer(audioUri);
+        } catch (Exception e) {
             Log.e("MP3", e.toString());
         }
     }
 
-    public static void stopAudio()
-    {
-        isAudioPlaying = false;
-        isAudioLoaded = false;
-        isAudioStopped = true;
+    public static synchronized void stopAudio() {
         if (mp != null) {
-            if(mp.isPlaying()) {
-                mp.reset();
+            if (mp.isPlaying()) {
                 mp.stop();
             }
+            mp.reset();
             mp.release();
             mp = null;
         }
-        return;
+        resetAudioFlags();
     }
 
-    public static void startAudio()
-    {
-        if (mp != null){
+    public static synchronized void startAudio() {
+        if (mp != null) {
+            mp.start();
             isAudioPlaying = true;
             isAudioStopped = false;
-            mp.start();
         }
     }
 
-    public static void pauseAudio()
-    {
-        if (isAudioPlaying)
-        {
+    public static synchronized void pauseAudio() {
+        if (mp != null && isAudioPlaying) {
+            mp.pause();
             isAudioPlaying = false;
             isAudioStopped = false;
-            mp.pause();
         }
     }
 
-    public static void resumeAudio()
-    {
+    public static synchronized void resumeAudio() {
         if (mp != null) {
-            int i = mp.getCurrentPosition();
-            mp.seekTo(i);
             mp.start();
             isAudioPlaying = true;
             isAudioStopped = false;
         }
     }
 
-    public static int getDuration(){
-        int duration = 0;
-        if (mp != null) {
-            duration = mp.getDuration();
-        }
-        return duration;
+    public static synchronized int getDuration() {
+        return mp != null ? mp.getDuration() : 0;
     }
 
-    public static int getCurrentPosition(){
-        int position = 0;
-        if (mp != null) {
-            position = mp.getCurrentPosition();
-        }
-        return position;
+    public static synchronized int getCurrentPosition() {
+        return mp != null ? mp.getCurrentPosition() : 0;
     }
 
-    public static void seekTo(int time){
+    public static synchronized void seekTo(int time) {
         if (mp != null) {
             mp.seekTo(time);
         }
     }
 
-    public static boolean isLoadedAudio(){
+    public static synchronized boolean isLoadedAudio() {
         return isAudioLoaded;
     }
 
-    public static boolean isStopped(){
-        if (mp != null && !mp.isPlaying()) {
-            isAudioStopped = true;
-        }
+    public static synchronized boolean isStopped() {
+        isAudioStopped = mp == null || !mp.isPlaying();
         return isAudioStopped;
     }
 
-    public static String getAudioUri(){
+    public static synchronized String getAudioUri() {
         return mp3Uri;
     }
 
+    private static void initializeMediaPlayer(String audioUri) throws Exception {
+        if (mp != null) {
+            mp.reset();
+            mp.release();
+        }
+        mp = new MediaPlayer();
+        mp.setDataSource(audioUri);
+        mp.prepare();
+        isAudioPlaying = false;
+        isAudioLoaded = true;
+        isAudioStopped = false;
+    }
+
+    private static void resetAudioFlags() {
+        isAudioPlaying = false;
+        isAudioLoaded = false;
+        isAudioStopped = true;
+        mp3Uri = null;
+    }
 }
