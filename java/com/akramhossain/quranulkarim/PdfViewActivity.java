6f0e4d02864ece0c3f84963b2b464e93d0d9ee9a
akram_rana <akram686202@gmail.com>
2025-07-19 14:08:47 +0600

    changes

---
+++ java/com/akramhossain/quranulkarim/PdfViewActivity.java
@@ -0,0 +1,81 @@
+package com.akramhossain.quranulkarim;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.github.barteksc.pdfviewer.PDFView;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+
+import java.io.File;
+
+public class PdfViewActivity extends AppCompatActivity {
+
+    PDFView pdfView;
+    private TextView pageInfo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+
+        setContentView(R.layout.activity_pdf_view);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        pageInfo = findViewById(R.id.pageInfo);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) ab.setDisplayHomeAsUpEnabled(true);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        //
+        pdfView = findViewById(R.id.pdfView);
+
+        String filePath = getIntent().getStringExtra("pdf_path");
+
+        Log.d("PDF_FILE",filePath);
+
+        if (filePath != null) {
+            File file = new File(filePath);
+            if (file.exists()) {
+                pdfView.fromFile(file)
+                        .enableSwipe(true)
+                        .enableDoubletap(true)
+                        .enableAnnotationRendering(true)
+                        .nightMode(false) // set true for dark background
+                        .onPageChange((page, pageCount) -> {
+                            pageInfo.setText("Page " + (page + 1) + " of " + pageCount);
+                        })
+                        .load();
+            } else {
+                Toast.makeText(this, "File not found", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        } else {
+            Toast.makeText(this, "Invalid file path", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    // Handle back button in toolbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close the activity
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- java/com/akramhossain/quranulkarim/adapter/PdfBookViewAdapter.java
+++ java/com/akramhossain/quranulkarim/adapter/PdfBookViewAdapter.java
@@ -1,22 +1,34 @@
 package com.akramhossain.quranulkarim.adapter;
 
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.akramhossain.quranulkarim.PdfViewActivity;
 import com.akramhossain.quranulkarim.R;
 import com.akramhossain.quranulkarim.model.TafsirPdfList;
 import com.akramhossain.quranulkarim.util.Utils;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
 import java.util.ArrayList;
 
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class PdfBookViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
@@ -54,6 +66,20 @@ public class PdfBookViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         rvHolder.file_name.setText(tb.getFile_name());
         rvHolder.url.setText(tb.getUrl());
         Picasso.get().load(tb.getThumb()).into(rvHolder.thumb);
+        //
+        File localFile = new File(c.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), tb.getFile_name());
+        if (localFile.exists()) {
+            rvHolder.actionIcon.setImageResource(android.R.drawable.ic_menu_view); // Open icon
+        } else {
+            rvHolder.actionIcon.setImageResource(android.R.drawable.stat_sys_download); // Download icon
+        }
+        holder.itemView.setOnClickListener(v -> {
+            if (localFile.exists()) {
+                openPdf(localFile);
+            } else {
+                downloadAndOpen(tb.getUrl(), tb.getFile_name());
+            }
+        });
     }
 
     @Override
@@ -61,6 +87,41 @@ public class PdfBookViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         return pdfBookList.size();
     }
 
+    private void openPdf(File file) {
+        //Log.d("PDF_FILE",file.getAbsolutePath());
+        Intent intent = new Intent(c, PdfViewActivity.class);
+        intent.putExtra("pdf_path", file.getAbsolutePath());
+        c.startActivity(intent);
+    }
+
+    private void downloadAndOpen(String url, String fileName) {
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setTitle("Downloading " + fileName);
+        request.setDescription("Please wait...");
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalFilesDir(c, Environment.DIRECTORY_DOWNLOADS, fileName);
+
+        DownloadManager manager = (DownloadManager) c.getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
+        Toast.makeText(c, "Downloading started...", Toast.LENGTH_SHORT).show();
+        // Optional: Listen for completion and auto-open
+        BroadcastReceiver onComplete = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context ctx, Intent intent) {
+                File file = new File(c.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), fileName);
+                if (file.exists()) {
+                    openPdf(file);
+                }
+                c.unregisterReceiver(this);
+            }
+        };
+        ContextCompat.registerReceiver(
+                c,
+                onComplete,
+                new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE),
+                ContextCompat.RECEIVER_NOT_EXPORTED
+        );
+    }
 
     class RecyclerViewHolder extends RecyclerView.ViewHolder {
         TextView name_bangla;
@@ -68,6 +129,7 @@ public class PdfBookViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         TextView file_name;
         TextView url;
         ImageView thumb;
+        ImageView actionIcon;
 
         public RecyclerViewHolder(View itemView) {
             super(itemView);
@@ -76,6 +138,7 @@ public class PdfBookViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
             file_name = (TextView) itemView.findViewById(R.id.file_name);
             url = (TextView) itemView.findViewById(R.id.url);
             thumb = (ImageView) itemView.findViewById(R.id.thumb);
+            actionIcon = itemView.findViewById(R.id.actionIcon);
         }
     }
 }
